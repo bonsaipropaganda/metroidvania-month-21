df064e6832dd7939784ded296611fcd92b8c933f
Coffe <jonathan_davey@outlook.com>
2023-08-26 11:55:21 +1000

    Reworked camera snapping

--- code/room_template.gd
+++ code/room_template.gd
@@ -1,17 +1,37 @@
-extends TileMap
+extends Area2D
 
+const MIN_ROOM_WIDTH = 640
+const MIN_ROOM_HEIGHT = 360
 
-# Called when the node enters the scene tree for the first time.
+const UP_TRANSITION_BOOST = -400
+
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+	if ($CollisionShape2D.position != Vector2.ZERO):
+		push_error("Room [" + name + "]'s collision shape has an offset. Please move it to position (0,0)")
+	if ($CollisionShape2D.shape.size.x < MIN_ROOM_WIDTH):
+		push_error("Room [" + name + "]'s width is smaller than the minimum")
+	if ($CollisionShape2D.shape.size.y < MIN_ROOM_HEIGHT):
+		push_error("Room [" + name + "]'s height is smaller than the minimum")
 
 func _on_body_entered(body):
-	if body.name == "Player":
+	if body.is_in_group("player"):
 		Global.current_room = self
 		Global.room_changed.emit(self)
+		snap_player_to_room()
+
+const snap_fatness = 20
+const snap_up_height = 50
+const snap_down_height = 50
+func snap_player_to_room():
+	if Global.get_player().global_position.x - snap_fatness < Global.get_cam().limit_left:
+		Global.get_player().global_position.x = Global.get_cam().limit_left + snap_fatness
+	elif Global.get_player().global_position.x + snap_fatness > Global.get_cam().limit_right:
+		Global.get_player().global_position.x = Global.get_cam().limit_right - snap_fatness
+	
+	if Global.get_player().global_position.y - snap_down_height < Global.get_cam().limit_top:
+		Global.get_player().global_position.y = Global.get_cam().limit_top + snap_down_height
+	elif Global.get_player().global_position.y + snap_up_height > (Global.get_cam().limit_bottom):
+		Global.get_player().global_position.y = Global.get_cam().limit_bottom - snap_up_height
+		if Global.get_player().velocity.y > UP_TRANSITION_BOOST:
+			Global.get_player().velocity.y = UP_TRANSITION_BOOST
+			# TODO: Letting go of jump shouldn't nullify new velocity
