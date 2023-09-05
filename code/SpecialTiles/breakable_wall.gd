1ee436266b648eaf2ecb2644faf4c09d8b6f4b18
Seth Louis <sethvihanlouis@gmail.com>
2023-09-04 23:44:47 -0400

    added breakable wall

---
+++ code/SpecialTiles/breakable_wall.gd
@@ -0,0 +1,19 @@
+extends StaticBody2D
+
+signal health_changed
+
+func _ready():
+	health_changed.connect(_on_health_changed)
+
+var current_health = 2:
+	set(value):
+		current_health = value
+		health_changed.emit()
+
+func _on_projectile_detector_area_entered(area):
+	if area.is_in_group("Projectiles"):
+		current_health -= 1
+
+func _on_health_changed():
+	if current_health <= 0:
+		queue_free()
