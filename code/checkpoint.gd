2c6c6a583da52e2f70471bb4e6fcc203253bd175
Seth Louis <sethvihanlouis@gmail.com>
2023-09-06 00:07:08 -0400

    start of checkpoint? might need to revert

--- code/checkpoint.gd
+++ code/checkpoint.gd
@@ -1,2 +1,19 @@
 extends Area2D
 
+var player_present = false
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		player_present = true
+
+func _on_body_exited(body):
+	if body.is_in_group("player"):
+		player_present = false
+
+func _process(delta):
+	if player_present:
+		if Input.is_action_just_pressed("action"):
+			set_checkpoint()
+
+func set_checkpoint():
+	Global.player_checkpoint = self.position
