672f7ee6cced62d076a7500981862b71fa915b11
Coffe <jonathan_davey@outlook.com>
2023-09-16 00:44:35 +1000

    add main menu

---
+++ code/main_menu.gd
@@ -0,0 +1,15 @@
+extends Sprite2D
+@onready var main = preload("res://scenes/state_machine/main.tscn")
+
+func _on_play_pressed():
+	get_tree().change_scene_to_packed(main)
+
+func _on_credits_pressed():
+	$Camera2D.position.x = 1280 / 2
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_go_back_pressed():
+	$Camera2D.position.x = 0
